eede9b6485f4d6ff6623a7e29490f1ec9eb80f63
Kyaw Aung Win <kyawagwin@gmail.com>
2018-05-08 22:09:40 +0800

    Routes Files with Express Router

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const mongoDbURI = require('./config/keys').mongoDbURI;
+
+const app = express();
 
 mongoose
   .connect(mongoDbURI)
   .then(() => console.log('MongoDB connected!'))
   .catch(err => console.log(err));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
